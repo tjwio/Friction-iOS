83df6dae25e6778e4f2264e579282adf3fc16431
Tim Wong <tim@tjw.io>
2018-11-25 16:38:55 -0800

    initial login/signup/welcome commit

---
+++ friction/Model/Extensions/UIImage+Constants.swift
@@ -0,0 +1,24 @@
+//
+//  UIImageView+Constants.swift
+//  Bump
+//
+//  Created by Tim Wong on 7/22/18.
+//  Copyright © 2018 tjwio. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    class var backArrowDark: UIImage? {
+        return UIImage(named: "back_arrow_dark")
+    }
+    
+    class var backArrowWhite: UIImage? {
+        return UIImage(named: "back_arrow_white")
+    }
+    
+    class var launchImageBg: UIImage? {
+        return UIImage(named: "launch_image_bg")
+    }
+}
